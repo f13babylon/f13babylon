6831d3885e5cdfcb78b0d85d190ad74d6244e1ad
ZephyrTFA <matthew@tfaluc.com>
2024-02-01 19:54:37 -0500

    yuh

--- tools/ci/install_rust_g.sh
+++ tools/ci/install_rust_g.sh
@@ -24,12 +24,13 @@ git checkout ${RUST_G_VERSION}
 
 # check for rust-g already being compiled with the wanted version
 hash_file=~/rust-g/HASH.$RUST_G_VERSION
-if [ -f "target/i686-unknown-linux-gnu/release/librust_g.so" ] && [ -f "$hash_file" ]; then
+cache_file=~/rust-g/librust_g.so
+if [ -f "$cache_file" ] && [ -f "$hash_file" ]; then
 	hash_wanted=$(cat "$hash_file")
-	hash_current=$(sha256sum "target/i686-unknown-linux-gnu/release/librust_g.so" | cut -d ' ' -f 1)
+	hash_current=$(sha256sum "$cache_file" | cut -d ' ' -f 1)
 	if [ "$hash_wanted" -eq "$hash_current" ]; then
-		echo "Rust-G already compiled and hashes match."
-		cp target/i686-unknown-linux-gnu/release/librust_g.so "$cwd/librust_g.so"
+		echo "Using cached rust-g"
+		cp "$cache_file" "$cwd/librust_g.so"
 		cd "$cwd"
 		exit 0
 	fi
@@ -38,8 +39,8 @@ fi
 rustup target add i686-unknown-linux-gnu
 export PKG_CONFIG_ALLOW_CROSS=1
 cargo build --release --target i686-unknown-linux-gnu
-echo $(sha256sum "target/i686-unknown-linux-gnu/release/librust_g.so" | cut -d ' ' -f 1) > "$hash_file"
-
+sha256sum target/i686-unknown-linux-gnu/release/librust_g.so | cut -d ' ' -f 1 > "$hash_file"
+cp target/i686-unknown-linux-gnu/release/librust_g.so "$cache_file"
 cp target/i686-unknown-linux-gnu/release/librust_g.so "$cwd/librust_g.so"
 cd "$cwd"
 chmod +x librust_g.so
